3ddac780262a4e06e0ab6ad18f3cabbaffc174a6
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2023-05-29 15:23:28 +0300

    Creates home controller and factories/seeders

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Post::factory()->count(50)->create();
     }
 }
